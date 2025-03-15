4e2496e9edc97d06e45e65ce193da345456c79d9
114bft68 <168547332+114bft68@users.noreply.github.com>
2025-03-15 15:58:24 +0800

    bad but I don't care

--- runCommand.c
+++ runCommand.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
 
-int main() {
-    system("start http://localhost:1234");
+int main(void) {
+    #ifdef _WIN32
+        system("start http://localhost:1234");
+    #elif __APPLE__
+        system("open https://localhost:1234");
+    #else
+        system("xdg-open https://localhost:1234");
+    #endif
+    
     system("node \"<PATH_TO_JS.JS>\"");
     return 0;
 }
